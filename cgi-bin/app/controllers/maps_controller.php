a41383a2f355dc1fd7c23e45bc27de4804871201
Daniel Cassman <dcassman@gmail.com>
2011-07-23 10:22:02 -0700

    Closes gh-5

--- cgi-bin/app/controllers/maps_controller.php
+++ cgi-bin/app/controllers/maps_controller.php
@@ -21,12 +21,19 @@ class MapsController extends AppController {
 		$this->layout = 'map';
 	}
 
+	/* Function: jsondata
+	 * ------------------
+	 * The critical map function that collects all the map data from the database
+	 * and parses it to JSON for the map javascript engine.
+	 */
 	function jsondata($id = null)	{
 		$this->layout = 'ajax';
 		$data = array();
 		$this->Map->id = $id;
 		$map = $this->Map->read();
 		$data['Map'] = $map['Map'];
+		
+		// Find the profiles for all the groups on the map
 		$groups = $this->Profile->find('all', array(
 			'fields'			=>	array('id','wikiid','name','shortname','startdate','enddate','leadership','majorattacks','short'),
 			'conditions'		=>	array("id IN (SELECT org_id FROM map_orgs WHERE map_id = $id)")
@@ -36,17 +43,23 @@ class MapsController extends AppController {
 			'fields'			=>	array('id','name','shortname'),
 			'conditions'		=>	array("id NOT IN (SELECT org_id FROM map_orgs WHERE map_id = $id)")
 		)));
+		
+		// Get additional zoom and position data for each group
 		$this->set('groupdata', $this->Map_org->find('all', array(
 			'fields'			=>	array('org_id','min_zoom','max_zoom','position'),
 			'conditions'		=>	array('map_id' => $id),
 			'order'				=>	'Map_org.position'
 		)));
+		
+		// Find all the links for this map
 		$data['links'] = $this->Link->find('all', array(
 			'conditions'		=>	array(
 				"group1 IN (SELECT org_id FROM map_orgs WHERE map_id = $id)",
 				"group2 IN (SELECT org_id FROM map_orgs WHERE map_id = $id)"
 			)
 		));
+		
+		// Find all the umbrellas for this map
 		$all_umbrellas = $this->Umbrella->find('all');
 		$umbrellas = array();
 		foreach($all_umbrellas as $umbrella)	{
