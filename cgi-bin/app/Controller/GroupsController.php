2475cb786fb798716a5f5e3614a5564d944957d4
Daniel Cassman <dcassman@gmail.com>
2012-08-24 20:42:11 -0800

    Closes gh-63; closes gh-62; closes gh-57; closes gh-56

--- cgi-bin/app/Controller/GroupsController.php
+++ cgi-bin/app/Controller/GroupsController.php
@@ -1,7 +1,7 @@
 <?php
 class GroupsController extends AppController {
 	public $name = 'Groups';
-	public $uses = array('Group','Link');
+	public $uses = array('Group','Link', 'Map');
 		
 	function index()	{
 		$this->set('title_for_layout', 'Profiles | Mapping Militant Organizations');
@@ -28,109 +28,188 @@ class GroupsController extends AppController {
 			'conditions'	=>	array('id IN (SELECT group_id FROM map_groups WHERE map_id IN (SELECT id FROM maps WHERE maps.published = 1))'),
 			'order'			=>	'name'
 		)));
+		$this->set('maps', $this->Map->find('all', array(
+			'conditions'	=>	array('published' => 1),
+			'fields'		=>	array('id','name')
+		)));
 		$this->set('title_for_layout', 'Export Data');
 	}
 	
 	function csv()	{
 		$this->layout = 'ajax';
-		$group_ids = $_POST['groups'];
-		$groups = array();
-		$columns = $_POST['columns'];
-		$csv = array('id,name,' . implode(',', $columns));
-		foreach($group_ids as $id)	{
-			$group = $this->Group->findById($id);
-			$links = $this->Link->find('all', array(
-				'conditions'	=>	"group1=$id OR group2=$id"
+		//$this->set('format', $_POST['format']);
+		if(isset($_POST['groups']) && $_POST['groups'] != null)	{
+			$group_ids = $_POST['groups'];
+		}	else	{
+			$group_ids = array();
+		}
+		if(isset($_POST['maps']))	{
+			$maps = $_POST['maps'];
+		}	else	{
+			$maps = array();
+		}
+		$all_groups = array();
+		foreach($maps as $id)	{
+			$map_groups = $this->Group->find('all', array(
+				'conditions'		=>	array("id IN (SELECT group_id FROM map_groups WHERE map_id = $id)")
 			));
-			$group['Link'] = $links;
-			//$groups[$id] = $group;
-			array_push($csv, $group['Group']['id'] . ',"' . $group['Group']['name'] . '"');
+			foreach($map_groups as $group)	{
+				if(!isset($all_groups[$group['Group']['id']]))	{
+					$links = $this->Link->find('all', array(
+						'conditions'	=>	'group1=' . $group['Group']['id'] . ' OR group2=' . $group['Group']['id']
+					));
+					$group['Link'] = $links;
+					$all_groups[$group['Group']['id']] = $group;
+				}
+			}
+		}
+		foreach($group_ids as $id)	{
+			if(!isset($all_groups[$id]))	{
+				$group = $this->Group->findById($id);
+				$links = $this->Link->find('all', array(
+					'conditions'	=>	"group1=$id OR group2=$id"
+				));
+				$group['Link'] = $links;
+				$all_groups[$id] = $group;
+			}
+		}
+		$groups = array();
+		foreach($all_groups as $group)	{
 			array_push($groups, $group);
 		}
-		foreach($columns as $col)	{
-			$group_count = 1;
-			foreach($groups as $group)	{
-				if($col == 'active_bool')	{
-					$csv[$group_count] .= ',' . ($group['Group']['enddate'] == '0000-00-00' ? 1 : 0);
+		//$this->set('groups', $groups);
+		if(isset($_POST['vars']))	{
+			$vars = $_POST['vars'];
+		}	else	{
+			$vars = array();
+		}
+		
+		$csv = array(array('id','name'));
+		for($i = 0; $i < count($groups); $i++)	{
+			array_push($csv, array($groups[$i]['Group']['id'], $groups[$i]['Group']['name']));
+		}
+		foreach($vars as $var)	{
+			array_push($csv[0], $var);
+			if($var == 'start_year')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					array_push($csv[$i + 1], substr($groups[$i]['Group']['startdate'], 0, 4));
 				}
-				if($col == 'start_year')	{
-					$csv[$group_count] .= ',' . substr($group['Group']['startdate'], 0, 4);
+			}
+			if($var == 'end_year')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					array_push($csv[$i + 1], ($groups[$i]['Group']['enddate'] == '0000-00-00' ? date('Y') : substr($groups[$i]['Group']['enddate'], 0, 4)));
 				}
-				if($col == 'start_year')	{
-				$csv[$group_count] .= ',' . ($group['Group']['enddate'] == '0000-00-00' ? date('Y') : substr($group['Group']['enddate'], 0, 4));
+			}
+			if($var == 'active')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					array_push($csv[$i + 1], ($groups[$i]['Group']['enddate'] == '0000-00-00' ? 1 : 0));
 				}
-				if($col == 'years_active')	{
-					$years_active = ($group['Group']['enddate'] == '0000-00-00' ? date('Y') : substr($group['Group']['enddate'], 0, 4))
-						- substr($group['Group']['startdate'], 0, 4);
-					$csv[$group_count] .= ',' . $years_active;
+			}
+			if($var == 'years_active')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					$years_active = ($groups[$i]['Group']['enddate'] == '0000-00-00' ? date('Y') : substr($groups[$i]['Group']['enddate'], 0, 4))
+						- substr($groups[$i]['Group']['startdate'], 0, 4);
+					array_push($csv[$i + 1], $years_active);
 				}
-				if($col == 'num_leaders')	{
-					$csv[$group_count] .= ',' . count($group['Leader']);
+			}
+			if($var == 'num_leaders')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					array_push($csv[$i + 1], count($groups[$i]['Leader']));
 				}
-				if($col == 'leader_duration')	{
+			}
+			if($var == 'avg_leader_duration')	{
+				for($i = 0; $i < count($groups); $i++)	{
 					$sum = 0;
 					$count = 0;
-					foreach($group['Leader'] as $leader)	{
+					foreach($groups[$i]['Leader'] as $leader)	{
 						if($leader['startdate'] == '0000-00-00' || $leader['enddate'] == '0000-00-00')
 							continue;
-						$sum += (substr($leader['enddate'], 0, 4) - substr($leader['startdate'], 0, 4));
+						$sum += (($leader['enddate'] == '0001-00-00' ? date('Y') : substr($leader['enddate'], 0, 4)) - substr($leader['startdate'], 0, 4));
 						$count ++;
 					}
 					$average = '""';
 					if($sum != 0 && $count != 0)	{
-						$average = $sum / $count;
+						$average = round($sum / $count, 4);
 					}
-					$csv[$group_count] .= ',' . $average;
+					array_push($csv[$i + 1], $average);
 				}
-				if($col == 'num_countries')	{
-					$csv[$group_count] .= ',' . count($group['Region']);
+			}
+			if($var == 'num_countries')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					array_push($csv[$i + 1], count($groups[$i]['Region']));
 				}
-				if($col == 'num_attacks')	{
-					$csv[$group_count] .= ',' . count($group['Attack']);
+			}
+			if($var == 'num_attacks')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					array_push($csv[$i + 1], count($groups[$i]['Attack']));
 				}
-				if($col == 'attacks_per_year')	{
-					$total = count($group['Attack']);
-					$years_active = ($group['Group']['enddate'] == '0000-00-00' ? date('Y') : substr($group['Group']['enddate'], 0, 4))
-						- substr($group['Group']['startdate'], 0, 4);
-					$average = $total / $years_active;
-					$csv[$group_count] .= ',' . $average;
+			}
+			if($var == 'attacks_per_year')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					$total = count($groups[$i]['Attack']);
+					$years_active = ($groups[$i]['Group']['enddate'] == '0000-00-00' ? date('Y') : substr($groups[$i]['Group']['enddate'], 0, 4))
+						- substr($groups[$i]['Group']['startdate'], 0, 4);
+					$average = '""';
+					if($years_active > 0)
+						$average = round($total / $years_active, 4);
+					array_push($csv[$i + 1], $average);
 				}
-				if($col == 'num_relationships')	{
-					$csv[$group_count] .= ',' . count($group['Link']);
+			}
+			if($var == 'num_relationships')	{
+				for($i = 0; $i < count($groups); $i++)	{
+					array_push($csv[$i + 1], count($groups[$i]['Link']));
 				}
-				if($col == 'num_splinters')	{
+			}
+			if($var == 'num_splinters')	{
+				for($i = 0; $i < count($groups); $i++)	{
 					$count = 0;
-					foreach($group['Link'] as $link)	{
-						if($link['Link']['type'] == 'spl' && $link['Link']['group1'] == $group['Group']['id'])
+					foreach($groups[$i]['Link'] as $link)	{
+						if($link['Link']['type'] == 'spl' && $link['Link']['group1'] == $groups[$i]['Group']['id'])
 							$count ++;
 					}
-					$csv[$group_count] .= ',' . $count;
+					array_push($csv[$i + 1], $count);
 				}
-				if($col == 'num_allies_affiliates')	{
+			}
+			if($var == 'num_allies_affiliates')	{
+				for($i = 0; $i < count($groups); $i++)	{
 					$count = 0;
-					foreach($group['Link'] as $link)	{
+					foreach($groups[$i]['Link'] as $link)	{
 						if($link['Link']['type'] == 'all' || $link['Link']['type'] == 'aff')
 							$count ++;
 					}
-					$csv[$group_count] .= ',' . $count;
+					array_push($csv[$i + 1], $count);
 				}
-				if($col == 'num_rivals')	{
+			}
+			if($var == 'num_rivals')	{
+				for($i = 0; $i < count($groups); $i++)	{
 					$count = 0;
-					foreach($group['Link'] as $link)	{
+					foreach($groups[$i]['Link'] as $link)	{
 						if($link['Link']['type'] == 'riv')
 							$count ++;
 					}
-					$csv[$group_count] .= ',' . $count;
+					array_push($csv[$i + 1], $count);
 				}
-				$group_count ++;
 			}
 		}
-		$filename = date('YmdHis') . rand(10000,99999) . '.csv';
-		$myFile = '../webroot/files/csv/' . $filename;
-		$fh = fopen($myFile, 'w') or die("can't open file");
-		fwrite($fh, implode("\n", $csv));
-		fclose($fh);
-		$this->set('path', $this->webroot . 'files/csv/' . $filename);
+		
+		if($_POST['format'] == 'csv')	{
+			for($i = 0; $i < count($csv); $i++)	{
+				$csv[$i] = implode(',', $csv[$i]);
+			}
+			$filename = date('YmdHis') . rand(10000,99999) . '.csv';
+			$myFile = '../webroot/files/csv/' . $filename;
+			$fh = fopen($myFile, 'w') or die("can't open file");
+			fwrite($fh, implode("\n", $csv));
+			fclose($fh);
+			$this->set('output', $this->webroot . 'files/csv/' . $filename);
+		}	else if($_POST['format'] == 'html')	{
+			$csv[0] = '<thead><tr><th>' . implode('</th><th>', $csv[0]) . '</th></tr></thead><tbody>';
+			for($i = 1; $i < count($csv); $i++)	{
+				$csv[$i] = implode('</td><td>', $csv[$i]);
+				$csv[$i] = '<tr><td>' . $csv[$i] . '</td></tr>';
+			}
+			$this->set('output', implode($csv) . '</tbody>');
+		}
 	}
 	
 	function test($id = null)	{
--- cgi-bin/app/View/Groups/data.ctp
+++ cgi-bin/app/View/Groups/data.ctp
@@ -3,32 +3,82 @@
 	$this->Html->script(array('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js','select2.min'), array('inline'=>false));
 	echo $this->Html->css(array('buttons','select2'),'stylesheet',array('inline'=>false));
 ?>
+<style type="text/css">
+	div.scroll-wrapper h2	{
+		padding-top:		15px;
+	}
+	ul.checkbox-list	{
+		margin:				0;
+		padding:			0;
+		margin-bottom:		1em;
+	}
+	ul.checkbox-list li	{
+		list-style-type:	none;
+		display:			block;
+		float:				left;
+		width:				45%;
+		padding:			5px 0;
+	}
+	
+	ul.checkbox-list li input[type=checkbox]	{
+		float:				left;
+		margin-right:		5px;
+		margin-top:			5px;
+	}
+	
+	ul.checkbox-list li label	{
+		width:				auto;
+	}
+</style>
 <h1 class="span-24">Export Data</h1>
 <div class="span-15 colborder">
 	<form id="dataForm" name="dataForm" action="csv" method="post" enctype="multipart/form-data">
-	<h2>Select Groups</h2>
-	<select name="groups[]" id="groups" multiple="multiple" style="width:100%;">
-		<?php foreach($groups as $group): ?>
-		<option value="<?php echo $group['Group']['id']; ?>"><?php echo $group['Group']['name']; ?></option>
-		<?php endforeach; ?>
-	</select>
-	<h2>Select Columns</h2>
-	<select name="columns[]" id="columns" multiple="multiple" style="width:100%">
-		<option value="start_year">Start Year</option>
-		<option value="end_year">End Year</option>
-		<option value="active_bool">Still Active</option>
-		<option value="years_active">Total Years Active</option>
-		<option value="num_leaders">Number of Leaders</option>
-		<option value="leader_duration">Average Leader Duration</option>
-		<option value="num_countries">Number of Countries</option>
-		<option value="num_attacks">Number of Major Attacks</option>
-		<option value="attacks_per_year">Major Attaks Per Year</option>
-		<option value="num_relationships">Number of relationships with other groups</option>
-		<option value="num_splinters">Number of splinter groups</option>
-		<option value="num_allies_affiliates">Number of allies and affiliates</option>
-		<option value="num_rivals">Number of rivals</option>
-	</select>
-	<p><input type="submit" value="Go" id="get_data_button"></p>
+	<div class="scroll-wrapper">
+		<h2 id="groups-header">Select Groups Individually</h2>
+		<p>First, you need to select which groups to include in your data export. You can pick individual groups using the box below. You can select as many groups as you like by selecting them directly from the list, or you can begin typing a group's name, and the list will filter the groups by name to the letters you type.</p>
+		<p><select name="groups[]" id="groups" multiple="multiple" style="width:100%;">
+			<?php foreach($groups as $group): ?>
+			<option value="<?php echo $group['Group']['id']; ?>"><?php echo $group['Group']['name']; ?></option>
+			<?php endforeach; ?>
+		</select></p>
+		<h2>Select Groups by Map</h2>
+		<p>Alternatively, you can select all the groups from a map by choosing as many maps as you like from below:</p>
+		<p><select name="maps[]" id="maps" multiple="multiple" style="width:100%;">
+			<?php foreach($maps as $map): ?>
+			<option value="<?php echo $map['Map']['id']; ?>"><?php echo $map['Map']['name']; ?></option>
+			<?php endforeach; ?>
+		</select></p>
+		<p><a href="#variables-header" class="scroll-button button">Next: Select Variables</a></p>
+	</div>
+	<div class="scroll-wrapper">
+		<h2 id="variables-header">Select Variables</h2>
+		<p>Next, you need to choose which variables to include in your dataset.</p>
+		<p class="clearfix"><input type="checkbox" style="float:left;margin-right:5px; margin-top:5px;" name="select_all" id="select_all" /><label for="select_all">Select all</label></p>
+		<ul id="variables-list" class="checkbox-list clearfix">
+			<li><input type="checkbox" name="start_year" id="start_year" /> <label for="start_year">Start year</label></li>
+			<li><input type="checkbox" name="end_year" id="end_year" /> <label for="end_year">End year</label></li>
+			<li><input type="checkbox" name="active" id="active" /> <label for="active">Still active</label></li>
+			<li><input type="checkbox" name="years_active" id="years_active" /> <label for="years_active">Total years active</label></li>
+			<li><input type="checkbox" name="num_leaders" id="num_leaders" /> <label for="num_leaders">Number of leaders</label></li>
+			<li><input type="checkbox" name="avg_leader_duration" id="avg_leader_duration" /> <label for="avg_leader_duration">Average leader duration</label></li>
+			<li><input type="checkbox" name="num_countries" id="num_countries" /> <label for="num_countries">Number of countries</label></li>
+			<li><input type="checkbox" name="num_attacks" id="num_attacks" /> <label for="num_attacks">Number of major attacks</label></li>
+			<li><input type="checkbox" name="attacks_per_year" id="attacks_per_year" /> <label for="attacks_per_year">Major attacks per year</label></li>
+			<li><input type="checkbox" name="num_relationships" id="num_relationships" /> <label for="num_relationships">Number of relationships</label></li>
+			<li><input type="checkbox" name="num_splinters" id="num_splinters" /> <label for="num_splinters">Number of splinter groups</label></li>
+			<li><input type="checkbox" name="num_allies_affiliates" id="num_allies_affiliates" /> <label for="num_allies_affiliates">Number of allies/affiliates</label></li>
+			<li><input type="checkbox" name="num_rivals" id="num_rivals" /> <label for="num_rivals">Number of rivals</label></li>
+		</ul>
+		<p><a href="#groups-header" class="scroll-button button">Back: Select Groups</a><a href="#output-header" class="scroll-button button">Next: Choose Format</a></p>
+	</div>
+	<div class="scroll-wrapper">
+		<h2 id="output-header">Choose Output Format</h2>
+		<p><select name="format" id="format">
+			<option value="csv">.csv file (Excel, Stata, R, etc.)</option>
+			<option value="html">HTML table</option>
+		</select></p>
+		<p><a href="#variables-header" class="button scroll-button">Back: Select Variables</a><a href="#" id="get_data_button" class="blue button">Get my data</a></p>
+	</div>
 	</form>
 </div>
 <div class="span-8 last">
@@ -45,11 +95,92 @@
 	</p>
 </div>
 <script type="text/javascript">
-	$('select','#dataForm').select2();
-	$("#dataForm").submit(function()	{
-		$.post('csv', {groups:$('#groups').val(), columns:$('#columns').val()}, function(data)	{
-			location.href = data;
-		})
+	$(function()	{
+		$('html,body').scrollTop(0);
+		$('div.scroll-wrapper').css('min-height',$(window).innerHeight());
+		$('select','#dataForm').select2();
+		$('.scroll-button').click(function()	{
+			var target = $(this).attr('href');
+			console.log(target);
+			$('html,body').animate({
+				'scrollTop': $(target).offset().top
+			}, 'slow');
+			return false;
+		});
+		$("#select_all").click(function()	{
+			if($(this).is(":checked"))	{
+				$("input","#variables-list").each(function()	{
+					$(this).attr('checked','checked');
+				});
+			}	else	{
+				$("input","#variables-list").each(function()	{
+					$(this).removeAttr('checked');
+				});
+			}
+		});
+		$("input", "#variables-list").click(function()	{
+			if(!$(this).is(":checked"))	{
+				$("#select_all").removeAttr('checked');
+			}
+		});
+	});
+	
+	$("#get_data_button").click(function()	{
+		var post_data = {
+			'groups': ($('#groups').val() == null ? [] : $('#groups').val()),
+			'maps': ($('#maps').val() == null ? [] : $('#maps').val()),
+			'vars': [],
+			'format': $('#format').val()
+		}
+		var checkboxes = $('input', '#variables-list');
+		for(var i = 0; i < checkboxes.length; i++)	{
+			if($(checkboxes[i]).is(':checked'))	{
+				post_data.vars.push($(checkboxes[i]).attr('name'));;
+			}
+		}
+		$.post('csv', post_data, function(data)	{
+			if($('#format').val() == 'csv')	{
+				window.location.href = data;
+			}	else if($('#format').val() == 'html')	{
+				$('html,body').scrollTop(0);
+				console.log(data);
+				var elements = $('body').children().detach();
+				/*$('body').prepend($('<div/>', {
+					'css':	{
+						'position':	'absolute',
+						'top': 0,
+						'left': 0,
+						'width':'100%',
+						'height':'100%',
+						'background-color':'#fff',
+						'text-align':'center'
+					}
+				}));*/
+				$('body').append($('<table/>', {
+					'html': data,
+					'css': {
+						'margin':'0 auto 20px',
+						'width':'80%',
+						'background-color':'#fff'
+					}
+				}));
+				$('body').prepend($('<p/>', {
+					'class': 'clearfix',
+					'html': $('<a/>', {
+						'href': '#',
+						'class': 'button',
+						'text': 'Close',
+						'click': function()	{
+							$('body').empty().append(elements);
+						}
+					}),
+					'css':	{
+						'width': '80%',
+						'margin': '20px auto 10px'
+					}
+				}));
+			}
+		});
 		return false;
 	});
 </script>
